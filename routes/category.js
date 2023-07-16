b8e30446d8a1ec5d78bd7780231cfa10397e7e15
shemaabra <abrashema9@gmail.com>
2023-07-16 13:27:31 +0300

    category table and api routes

---
+++ routes/category.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const connection = require("../connection");
+const router = express.Router();
+var auth = require("../services/authentication");
+var checkRole = require("../services/checkRole");
+
+router.post(
+  "/add",
+  auth.authenticateToken,
+  checkRole.checkRole,
+  (req, res, next) => {
+    let category = req.body;
+    query = "insert into category (name) values(?)";
+    connection.query(query, [category.name], (err, results) => {
+      if (!err) {
+        return res.status(200).json({ message: "Category Added Successfully" });
+      } else {
+        return res.status(500).json(err);
+      }
+    });
+  }
+);
+
+router.get("/get", auth.authenticateToken, (req, res, next) => {
+  var query = "select *from category order by name";
+  connection.query(query, (err, results) => {
+    if (!err) {
+      return res.status(200).json(results);
+    } else {
+      return res.status(500).json(err);
+    }
+  });
+});
+
+
+router.patch("", auth.authenticateToken, checkRole.checkRole, (req, res, next)=>{
+    let  category = req.body;
+    query = "update category set name=? where id = ?";
+    connection.query(query, [category.name, category.id], (err, resutls)=>{
+        if (!err) {
+            if (res.affectedRows == 0) {
+                return res.status(400).json({message: "Category id does not exist"});
+            }else {
+
+            }
+        }else {
+            return res.status(500).json(err);
+        }
+    });
+});
--- table.sql
+++ table.sql
@@ -12,4 +12,10 @@ create table user(
 insert into user (name, contactNumber, email, password, status, role)values('admin', '0765904173', 'admin@gmail.com', '1223436', 'true', 'admin');
 
 
+create table category (
+    id int NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    primary key(id)
+);
+
 
