c9c1f2d36363240564366e8ab661e920603068af
shemaabra <abrashema9@gmail.com>
2023-07-17 21:48:39 +0300

    bills api

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 const express = require("express");
 const cors = require("cors");
-const connection = require('./connection');
-const userRoutes = require('./routes/user');
-const categoryRoutes = require('./routes/category');
-const productRoutes = require('./routes/product');
-
-
+const connection = require("./connection");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+const billRoutes = require("./routes/bills");
 
 const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/user', userRoutes);
-app.use('/category', categoryRoutes);
-app.use('/product', productRoutes);
-
-
-
+app.use("/user", userRoutes);
+app.use("/category", categoryRoutes);
+app.use("/product", productRoutes);
+app.use("/bill", billRoutes);
 
 module.exports = app;
